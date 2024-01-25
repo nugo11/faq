8f03b014208e244774966245324ea2c03d65f4f2
nugoi1 <nugoxucishvili@gmail.com>
2024-01-26 02:46:50 +0400

    fix arrow

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let span = document.getElementsByClassName("hide");
 let faq = document.getElementsByClassName("faq");
 let show = document.getElementsByTagName("h1");
+let arrow = document.getElementsByTagName('span');
 
 for (let i = 0; i < span.length; i++) {
   span[i].style.opacity = "0";
@@ -12,10 +13,12 @@ for (let i = 0; i < span.length; i++) {
       span[i].style.opacity = "1";
       span[i].style.position = "relative";
       show[i].style.fontWeight = "bold";
+      arrow[i].style.transform = "rotate(180deg)";
     } else {
       span[i].style.opacity = "0";
       span[i].style.position = "absolute";
       show[i].style.fontWeight = "normal";
+      arrow[i].style.transform = "none";
     }
   });
 }

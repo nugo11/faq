c95202ecf9ba833922ded97920cfdeaf75f43621
nugoi1 <nugoxucishvili@gmail.com>
2024-01-26 03:00:48 +0400

    add smooth animation and fix multiple opening bug

--- js/app.js
+++ js/app.js
@@ -4,19 +4,25 @@ let show = document.getElementsByTagName("h1");
 let arrow = document.getElementsByTagName('span');
 
 for (let i = 0; i < span.length; i++) {
-  span[i].style.opacity = "0";
-  span[i].style.position = "absolute";
-  span[i].style.transition = "0.2s";
+  span[i].style.height = "0";
+  span[i].style.overflow = "hidden";
+  span[i].style.transition = "height 0.2s ease-in-out";
 
   faq[i].addEventListener("click", () => {
-    if (span[i].style.opacity === "0") {
-      span[i].style.opacity = "1";
-      span[i].style.position = "relative";
+    for (let j = 0; j < span.length; j++) {
+      if (j !== i) {
+        span[j].style.height = "0";
+        show[j].style.fontWeight = "normal";
+        arrow[j].style.transform = "none";
+      }
+    }
+
+    if (span[i].style.height === "0px") {
+      span[i].style.height = span[i].scrollHeight + "px";
       show[i].style.fontWeight = "bold";
       arrow[i].style.transform = "rotate(180deg)";
     } else {
-      span[i].style.opacity = "0";
-      span[i].style.position = "absolute";
+      span[i].style.height = "0";
       show[i].style.fontWeight = "normal";
       arrow[i].style.transform = "none";
     }
